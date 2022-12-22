99a27c8711f9621d1d6228a999dd4d25efa0a398
Renz Landicho <renzrulandicho@gmail.com>
2022-12-22 11:33:45 +0800

    add: basic code for creating a book object and pushing it to the userLibrary array

---
+++ script.js
@@ -0,0 +1,19 @@
+let userLibrary = [];
+
+function addBook() {}
+
+addBook.prototype.newArr = function () {
+    userLibrary.push(this);
+}
+
+function Book(title, author, pages, status) {
+    this.title = title,
+        this.author = author,
+        this.pages = pages,
+        this.status = status
+}
+
+Book.prototype = Object.create(addBook.prototype);
+
+const book1 = new Book('Holes', 'Louis Sachar', 251, 'Read');
+book1.newArr();
